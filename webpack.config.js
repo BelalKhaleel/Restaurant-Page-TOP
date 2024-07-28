12573e25f5679ae624132923b09fbc69d09f5455
Belal <119034702+BelalKhaleel@users.noreply.github.com>
2024-07-28 15:38:35 +0300

    Add package.json and webpack.config files

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
@@ -11,5 +15,13 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
